b42de444ebe8ee636b9ed77e4ceef18df5c80878
Aleksey Radionov <pretorean@gmail.com>
2019-10-27 01:02:06 +0300

    assets icon

--- lib/ui/res/assets.dart
+++ lib/ui/res/assets.dart
@@ -5,3 +5,16 @@ const String androidMipMapIcon = "@mipmap/ic_launcher";
 // bottom bar icons
 const icHomeActivated = "$_baseResPath/ic_home_activated.svg";
 const icHomeInactive = "$_baseResPath/ic_home_inactive.svg";
+
+const icHeartActivated = "$_baseResPath/ic_heart_activated.svg";
+const icHeartInactive = "$_baseResPath/ic_heart_inactive.svg";
+
+const icProfileActivated = "$_baseResPath/ic_profile_activated.svg";
+const icProfileInactive = "$_baseResPath/ic_profile_inactive.svg";
+
+const icShopActivated = "$_baseResPath/ic_shop_activated.svg";
+const icShopInactive = "$_baseResPath/ic_shop_inactive.svg";
+
+const icBagActivated = "$_baseResPath/ic_bag_activated.svg";
+const icBagInactive = "$_baseResPath/ic_bag_inactive.svg";
+const icBagIndicatorInactive = "$_baseResPath/ic_bag_indicator_inactive.svg";
