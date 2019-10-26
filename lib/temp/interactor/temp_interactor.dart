cfe6996c8e544037abbb8e794ad1303a8bae1571
Aleksey Radionov <pretorean@gmail.com>
2019-10-26 22:38:47 +0300

    add screen and interactor template

---
+++ lib/temp/interactor/temp_interactor.dart
@@ -0,0 +1,8 @@
+import 'package:shop_app/temp/interactor/temp_repository/temp_repository.dart';
+
+/// Интерактор для работы с <todo>
+class TempInteractor {
+  final TempRepository repository;
+
+  TempInteractor(this.repository);
+}
