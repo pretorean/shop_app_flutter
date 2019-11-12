a9d55a19bab2d8cd9da7cf5c19fd9b6ca30ca2fa
Aleksey Radionov <pretorean@gmail.com>
2019-11-12 23:29:20 +0300

    add screen

--- lib/ui/screen/catalog/catalog_screen.dart
+++ lib/ui/screen/catalog/catalog_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:injector/injector.dart';
 import 'package:mwwm/mwwm.dart';
+import 'package:shop_app/domain/Category.dart';
 import 'package:shop_app/ui/common/bottom_bar_widget.dart';
+import 'package:shop_app/ui/common/category_card.dart';
+import 'package:shop_app/ui/res/strings/strings.dart';
 import 'package:shop_app/ui/screen/catalog/catalog_route.dart';
 import 'package:shop_app/ui/screen/catalog/catalog_wm.dart';
 import 'package:shop_app/ui/screen/catalog/di/catalog_component.dart';
@@ -23,20 +26,60 @@ class _CatalogScreenState extends WidgetState<CatalogWidgetModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: Injector.of<CatalogScreenComponent>(context).component.scaffoldKey,
-      appBar: AppBar(
-        title: Text("Catalog screen"),
-      ),
       bottomNavigationBar: BottomBarWidget(
         currentIndex: CatalogScreenRoute.thisPageIndex,
         bagIsEmpty: false,
       ),
-      body: _buildBody(),
+      body: _buildScrollBody(),
     );
   }
 
-  Widget _buildBody() {
-    return Container(
-      child: Text("Catalog screen"),
+  Widget _buildScrollBody() {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          title: Text(categoryScreenTitle),
+          pinned: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        EntityStateBuilder<List<Category>>(
+          streamedState: wm.categoryListState,
+          loadingChild: SliverList(
+            delegate: SliverChildListDelegate([
+              Center(
+                child: CircularProgressIndicator(),
+              ),
+            ]),
+          ),
+          errorChild: SizedBox.shrink(),
+          child: (context, List<Category> list) {
+            return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  Category item = list[index];
+                  return Padding(
+                    padding: EdgeInsets.only(
+                        top: 8.0, bottom: 8.0, left: 16.0, right: 16.0),
+                    child: CategoryCard(
+                      onTap: () {},
+                      categoryName: item.name,
+                      image: item.imgPath != null
+                          ? Image.asset(item.imgPath)
+                          : null,
+                    ),
+                  );
+                },
+                childCount: list.length,
+              ),
+            );
+          },
+        )
+      ],
     );
   }
 }
--- lib/ui/screen/catalog/catalog_wm.dart
+++ lib/ui/screen/catalog/catalog_wm.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/widgets.dart';
 import 'package:mwwm/mwwm.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:shop_app/domain/Category.dart';
+import 'package:shop_app/ui/res/assets.dart';
+
+class _InteractorMock {
+  Observable<List<Category>> getCategories() => Observable.just([
+        Category(
+          name: 'Новое',
+          imgPath: imgDemo18,
+        ),
+        Category(
+          name: 'Одежда',
+          imgPath: imgDemo15,
+        ),
+        Category(
+          name: 'Обувь',
+          imgPath: imgDemo16,
+        ),
+        Category(
+          name: 'Аксессуары',
+          imgPath: imgDemo17,
+        ),
+      ]).delay(Duration(milliseconds: 300));
+}
 
 /// [WidgetModel] для экрана <Catalog>
 class CatalogWidgetModel extends WidgetModel {
   final GlobalKey<NavigatorState> navigator;
+
+  final categoryListState = EntityStreamedState<List<Category>>();
+
+  _InteractorMock _interactor = _InteractorMock();
 
   CatalogWidgetModel(
     WidgetModelDependencies dependencies,
@@ -13,6 +41,7 @@ class CatalogWidgetModel extends WidgetModel {
   @override
   void onLoad() {
     super.onLoad();
-    //todo
+
+    subscribe(_interactor.getCategories(), categoryListState.content);
   }
 }
